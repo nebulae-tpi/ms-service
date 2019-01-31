5fd166537bc1e23ffdeb13653cad9f37c745fc4b
Felipe Santa <juan.santa@nebulae.com.co>
2019-01-31 11:39:03 -0500

    replicaset setup updated in compose, gke and frontend's were created

---
+++ frontend/emi/driver-vehicle-assignment/driver-vehicle-assignment.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../../core/modules/shared.module';
+import { DatePipe } from '@angular/common';
+import { FuseWidgetModule } from '../../../core/components/widget/widget.module';
+
+import { DriverService } from './driver-vehicle-assignment.service';
+import { DriverListService } from './driver-list/driver-list.service';
+import { DriverListComponent } from './driver-list/driver-list.component';
+import { VehicleAssignmentService } from './vehicle-assignment/vehicle-assignment.service';
+// import { VehicleAssignmentComponent } from './vehicle-assignment/assigments/assignments.component';
+import { VehicleAssignmentComponent } from './vehicle-assignment/vehicle-assignment.component';
+import { ToolbarService } from '../../toolbar/toolbar.service';
+import { DialogComponent } from './dialog/dialog.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DriverListComponent,
+  },
+  {
+    path: ':id',
+    component: VehicleAssignmentComponent,
+  }
+];
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes),
+    FuseWidgetModule
+  ],
+  declarations: [
+    DialogComponent,
+    DriverListComponent,
+    VehicleAssignmentComponent
+  ],
+  entryComponents: [DialogComponent],
+  providers: [ DriverService, DriverListService, VehicleAssignmentService, DatePipe]
+})
+
+export class DriverVehicleAssignmentModule {}
---
+++ frontend/emi/satellite/satellite.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../../../core/modules/shared.module';
+import { DatePipe } from '@angular/common';
+import { FuseWidgetModule } from '../../../core/components/widget/widget.module';
+
+import { SatelliteService } from './satellite.service';
+import { ToolbarService } from '../../toolbar/toolbar.service';
+import { DialogComponent } from './dialog/dialog.component';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(routes),
+    FuseWidgetModule
+  ],
+  declarations: [
+    DialogComponent
+  ],
+  entryComponents: [DialogComponent],
+  providers: [ SatelliteService, DatePipe]
+})
+
+export class SatelliteModule {}
