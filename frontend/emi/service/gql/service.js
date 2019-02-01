3d90bcef88fc144b5ba0732bebcc21e1c52a677c
Jaime Zapata <esteban.zapata@nebulae.com.co>
2019-02-01 17:28:29 -0500

    Creating historical service form

--- frontend/emi/service/gql/service.js
+++ frontend/emi/service/gql/service.js
@@ -1,37 +1,89 @@
 import gql from "graphql-tag";
 
+// FRAGMENTS
+export const ServiceServiceFieldsFragment = gql`
+    fragment ServiceServiceFieldsFragment on ServiceService {
+      _id
+      requestTimestamp
+      client {
+        name
+      }
+      driver {
+        driverId
+        name
+        documentType
+        documentId
+        username
+      }
+      vehicle {
+        vehicleId
+        licensePlate
+        model
+        brand
+        line
+      }
+      pickUp {
+        marker {
+          lat
+          lng
+        }
+        polygon {
+          lat
+          lng
+        }
+        city
+        zone
+        neighborhood
+        addressLine1
+        addressLine2
+        notes
+      }
+      dropOff {
+        marker {
+          lat
+          lng
+        }
+        polygon {
+          lat
+          lng
+        }
+        city
+        zone
+        neighborhood
+        addressLine1
+        addressLine2
+        notes
+      }
+      verificationCode
+      requestedFeatures
+      paymentType
+      fareDiscount
+      fare
+      route {
+        lat
+        lng
+      }
+      state
+      businessId
+    }
+  `;
+
 // We use the gql tag to parse our query string into a query document
 export const ServiceService = gql`
   query ServiceService($id: String!) {
     ServiceService(id: $id) {
-      _id
-      generalInfo {
-        name
-        description
-      }
-      state
-      creationTimestamp
-      creatorUser
-      modificationTimestamp
-      modifierUser
+      ...${ServiceServiceFieldsFragment}
     }
+    ${ServiceServiceFieldsFragment}
   }
 `;
 
 export const ServiceServices = gql`
   query ServiceServices($filterInput: FilterInput!, $paginationInput: PaginationInput!) {
     ServiceServices(filterInput: $filterInput, paginationInput: $paginationInput) {
-      _id
-      generalInfo {
-        name
-        description
-      }
-      state
-      creationTimestamp
-      creatorUser
-      modificationTimestamp
-      modifierUser
+      ...${ServiceServiceFieldsFragment}
     }
+    ${ServiceServiceFieldsFragment}
   }
 `;
 
@@ -45,16 +97,8 @@ export const ServiceServicesSize = gql`
 export const ServiceServiceUpdatedSubscription = gql`
   subscription{
     ServiceServiceUpdatedSubscription{
-      _id
-      generalInfo {
-        name
-        description
-      }
-      state
-      creationTimestamp
-      creatorUser
-      modificationTimestamp
-      modifierUser
+      ...${ServiceServiceFieldsFragment}
     }
+    ${ServiceServiceFieldsFragment}
   }
 `;
