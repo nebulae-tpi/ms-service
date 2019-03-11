6faf1c8a6552efab2b1d0d1fa364d4b53c4f2262
Felipe Santa <juan.santa@nebulae.com.co>
2019-03-11 17:33:13 -0500

    force deployment

--- frontend/emi/integrated-operating-environment/operator-workstation/datatable/datatable.component.ts
+++ frontend/emi/integrated-operating-environment/operator-workstation/datatable/datatable.component.ts
@@ -497,7 +497,7 @@ export class DatatableComponent implements OnInit, OnDestroy {
   }
 
   calcServiceEta(service) {
-    if (!service.pickUpETA || service.state.includes('CANCELLED')) {
+    if (!service.pickUpETA || service.state === 'DONE' || service.state.includes('CANCELLED') ) {
       return '---';
     }
 
