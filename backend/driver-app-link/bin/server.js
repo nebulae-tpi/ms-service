6ee161343664056ba4c349cc244e745c2dd94537
Felipe Santa <juan.santa@nebulae.com.co>
2019-03-12 16:00:22 -0500

    force deployment

--- backend/driver-app-link/bin/server.js
+++ backend/driver-app-link/bin/server.js
@@ -34,5 +34,4 @@ const start = () => {
     );
 };
 
-
 start();
--- frontend/emi/integrated-operating-environment/integrated-operating-environment.service.ts
+++ frontend/emi/integrated-operating-environment/integrated-operating-environment.service.ts
@@ -8,7 +8,6 @@ export class IntegratedOperatingEnvironmentService {
 
   constructor(private gateway: GatewayService) {
   }
-    
-
+   
 
 }
