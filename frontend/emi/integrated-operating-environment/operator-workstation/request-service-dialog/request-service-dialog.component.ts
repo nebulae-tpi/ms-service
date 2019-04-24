28f1583fe596d60f56c046a8893b69254c43ea52
juansantane <juan.santa@nebulae.com.co>
2019-04-24 12:44:47 -0500

    tip client is undefined by default &&  button to clear google place selected was removed

--- frontend/emi/integrated-operating-environment/operator-workstation/request-service-dialog/request-service-dialog.component.ts
+++ frontend/emi/integrated-operating-environment/operator-workstation/request-service-dialog/request-service-dialog.component.ts
@@ -342,7 +342,7 @@ export class RequestServiceDialogComponent implements OnInit, OnDestroy, AfterVi
                 ? SPECIAL_DESTINATION_PRICE_MODS[destinationOptionsGroup]
                 : client.satelliteInfo
                   ? client.satelliteInfo.tip
-                  : 0,
+                  : undefined,
             tipType: this.doorMenOptions
               ? this.doorMenOptions[this.selectedIndexDoorman].tipType
               : client.satelliteInfo ? client.satelliteInfo.tipType : '',
