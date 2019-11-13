e1241a4891e3b8757319cacf5f2c0bb27f70a556
Daniel Tobon <daniel.tobon@nebulae.com.co>
2019-11-13 15:06:32 -0500

    Se agrega logs de seguimiento

--- backend/driver-app-link/bin/domain/service/ServiceES.js
+++ backend/driver-app-link/bin/domain/service/ServiceES.js
@@ -204,6 +204,8 @@ class ServiceES {
         ).toPromise();
         //ignores shifts that were already taken into account
         shifts = shifts.filter(s => !Object.keys(service.offer.shifts).includes(s._id));
+        console.log('Shift 1 =============>  ', JSON.stringify(shifts));
+        console.log('=============================================================');
         //if the services requires VIRTUAL_WALLET balance, then filter everyone that does not have sufficent money
         shifts = service.client.tipType === "VIRTUAL_WALLET"
             ? shifts.filter(s =>
@@ -214,6 +216,8 @@ class ServiceES {
                 )
             )
             : shifts;
+        console.log('Shift 2 =============>  ', JSON.stringify(shifts));
+        console.log('=============================================================');
         obs.next(`raw shift candidates: ${JSON.stringify(shifts.map(s => ({ driver: s.driver.username, distance: s.dist.calculated, documentId: s.driver.documentId })))} `);
 
         // if the service has a referred driver and that driver is within the candidates, then that shift must be the first (high priority) 
@@ -225,6 +229,8 @@ class ServiceES {
                 obs.next(`referred found between candidates: ${JSON.stringify({ driver: priorityShift.driver.username, distance: priorityShift.dist.calculated, documentId: priorityShift.driver.documentId })} `);
             }
         }
+        console.log('Shift 3 =============>  ', JSON.stringify(shifts));
+        console.log('=============================================================');
         // filter all the trips that are closer than the minDistance threshold
         shifts = shifts.filter(s => {
             console.log('Shift Referred: ', s.referred);
