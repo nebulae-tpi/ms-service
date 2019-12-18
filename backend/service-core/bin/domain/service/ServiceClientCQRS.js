6d2bebed2c2ff7b1fcb7583b5ba39393250c00b0
juansantane <juan.santa@nebulae.com.co>
2019-12-18 18:01:37 -0500

    0% discount for requests with Client

--- backend/service-core/bin/domain/service/ServiceClientCQRS.js
+++ backend/service-core/bin/domain/service/ServiceClientCQRS.js
@@ -107,8 +107,9 @@ class ServiceClientCQRS {
     // const businessId = authToken.businessId;
     
     const { id, tripCost, client } = args;
-    args.fareDiscount = client ? 0 : 0.10;
+    args.fareDiscount = client ? 0 : 0; // second zero means 0% discount
     args.fareDiscount = (tripCost && tripCost > 0) ? 0 : args.fareDiscount;
+    
     // ServiceClientCQRS.log(`ServiceCQRS.requestServices RQST: ${JSON.stringify(args)}`); //DEBUG: DELETE LINE
     return RoleValidator.checkPermissions$(authToken.realm_access.roles, "service-core.ServiceCQRS", "requestServices", PERMISSION_DENIED, ["CLIENT"])
     .pipe( 
