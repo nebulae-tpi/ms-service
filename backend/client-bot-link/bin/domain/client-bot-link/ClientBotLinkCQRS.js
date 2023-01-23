96b826c1b5037ff5141b2c179b8bb086b8fdb136
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2023-01-23 09:23:10 -0500

    Se agregan logs de seguimiento

--- backend/client-bot-link/bin/domain/client-bot-link/ClientBotLinkCQRS.js
+++ backend/client-bot-link/bin/domain/client-bot-link/ClientBotLinkCQRS.js
@@ -22,112 +22,130 @@ let instance;
 
 class ClientBotLinkCQRS {
 
-    constructor() {
+  constructor() {
+  }
+
+
+  //#region Object builders
+
+  processMessageReceived$({ args }, authToken) {
+    console.log("MESSAGES ====> ", JSON.stringify(args));
+    if (args.messages) {
+      return from(args.messages).pipe(
+        tap(message => {
+          this.markMessageAsRead(message)
+          const content = {
+            "recipient_type": "individual",
+            "to": message.from,
+            "type": "text",
+            "text": {
+              "body": "ESTOY VIVO!!!!!"
+            }
+          }
+          const options = {
+            protocol: 'https:',
+            hostname: 'waba.360dialog.io',
+            path: '/v1/messages/',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'D360-API-KEY': process.env.D360_API_KEY,
+            }
+          }
+          const req = https.request(options, res => {
+            let data = ''
+
+            res.on('data', chunk => {
+              data += chunk
+            })
+
+            res.on('end', () => {
+              console.log(JSON.parse(data))
+            })
+          })
+            .on('error', err => {
+              console.log('Error: ', err.message)
+            })
+          req.write(JSON.stringify(content))
+          req.end();
+        })
+      )
+    } else {
+      return of("IGNORED")
     }
 
-    
-    //#region Object builders
+  }
 
-    processMessageReceived$({ args }, authToken) {
-        console.log("MESSAGES ====> ", JSON.stringify(args));
-        if(args.messages){
-            return from(args.messages).pipe(
-                tap(message => {
-                    const content = {
-                        "recipient_type": "individual",
-                        "to": message.from,
-                        "type": "text",
-                        "text": {
-                            "body": "ESTOY VIVO!!!!!"
-                        }
-                      }
-                    const options = {
-                        protocol: 'https:',
-                        hostname: 'waba.360dialog.io',
-                        path: '/v1/messages/',
-                        method: 'POST',
-                        headers: {
-                          'Content-Type': 'application/json',
-                          'D360-API-KEY': process.env.D360_API_KEY,
-                        }
-                      }
-                    const req = https.request(options, res => {
-                        let data = ''
-                    
-                        res.on('data', chunk => {
-                          data += chunk
-                        })
-                    
-                        res.on('end', () => {
-                          console.log(JSON.parse(data))
-                        })
-                      })
-                      .on('error', err => {
-                        console.log('Error: ', err.message)
-                      })
-                    req.write(JSON.stringify(content))
-                    req.end()
-                    // return defer(()=> {
-                    //     return fetch("https://waba.360dialog.io/v1/messages/", {
-                    //         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                    //         mode: 'cors', // no-cors, *cors, same-origin
-                    //         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                    //         headers: {
-                    //           'Content-Type': 'application/json',
-                    //           'D360-API-KEY': process.env.D360_API_KEY,
-                    //         },
-                    //         redirect: 'follow', // manual, *follow, error
-                    //         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                    //         body: JSON.stringify(content) 
-                    //       });
-                    // })
-                })
-            )
-        }else {
-            return of("IGNORED")
-        }
-        
+
+  markMessageAsRead(message) {
+    const content = {
+      "status": "read"
+    }
+    const options = {
+      protocol: 'https:',
+      hostname: 'waba.360dialog.io',
+      path: `/v1/messages/${message.id}`,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'D360-API-KEY': process.env.D360_API_KEY,
       }
-
-
-    //#endregion
-
-
-
-
-
-    //#region NOT BEING CALLED AT THE MOMENT
-
-
-    /**
-     * Logs an error at the console.error printing only the message and the stack related to the project source code
-     * @param {Error} error 
-     */
-    static logError(error) {
-        if (!error.stack) {
-            console.error(error);
-            return;
-        }
-        try {
-            const stackLines = error.stack.split('\n');
-            console.error(
-                new Date().toString() + ': ' + stackLines[0] + '\n' + stackLines.filter(line => line.includes('client-bot-link/bin')).join('\n') + '\n'
-            );
-        }
-        catch (e) {
-            console.error(e);
-            console.error(error);
-        }
     }
+    const req = https.request(options, res => {
+      let data = ''
+
+      res.on('data', chunk => {
+        data += chunk
+      })
+
+      res.on('end', () => {
+        console.log(JSON.parse(data))
+      })
+    })
+      .on('error', err => {
+        console.log('Error: ', err.message)
+      })
+    req.write(JSON.stringify(content))
+    req.end()
+  }
+  //#endregion
+
+
+
+
+
+  //#region NOT BEING CALLED AT THE MOMENT
+
+
+  /**
+   * Logs an error at the console.error printing only the message and the stack related to the project source code
+   * @param {Error} error 
+   */
+  static logError(error) {
+    if (!error.stack) {
+      console.error(error);
+      return;
+    }
+    try {
+      const stackLines = error.stack.split('\n');
+      console.error(
+        new Date().toString() + ': ' + stackLines[0] + '\n' + stackLines.filter(line => line.includes('client-bot-link/bin')).join('\n') + '\n'
+      );
+    }
+    catch (e) {
+      console.error(e);
+      console.error(error);
+    }
+  }
 }
 
 /**
  * @returns {ClientBotLinkCQRS}
  */
 module.exports = () => {
-    if (!instance) {
-        instance = new ClientBotLinkCQRS();
-        console.log(`${instance.constructor.name} Singleton created`);
-    }
-    return instance;
+  if (!instance) {
+    instance = new ClientBotLinkCQRS();
+    console.log(`${instance.constructor.name} Singleton created`);
+  }
+  return instance;
 };
