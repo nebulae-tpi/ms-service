4769d96c3a9e57db096b53bdd28016c19482a206
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2023-01-25 11:37:27 -0500

    Se crea proceso de solicitud de servicio

---
+++ backend/client-bot-link/bin/domain/client-bot-link/data-access/ClientDA.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let mongoDB = undefined;
+//const mongoDB = require('./MongoDB')();
+const CollectionName = "Client";
+const { CustomError } = require("../../../tools/customError");
+const { map, mergeMap, reduce } = require("rxjs/operators");
+const { of, Observable, defer, from } = require("rxjs");
+const Crosscutting = require("../../../tools/Crosscutting");
+
+class ClientDA {
+  static start$(mongoDbInstance) {
+    return Observable.create(observer => {
+      if (mongoDbInstance) {
+        mongoDB = mongoDbInstance;
+        observer.next("using given mongo instance");
+      } else {
+        mongoDB = require("../../../data/MongoDB").singleton();
+        observer.next("using singleton system-wide mongo instance");
+      }
+      observer.complete();
+    });
+  }
+
+    /**
+   * Gets a client satellite by the client id
+   */
+  static getClientByPhoneNumber$(waId) {
+    const collection = mongoDB.db.collection(CollectionName);
+
+    const query = {
+      "generalInfo.phone": waId
+    };
+    return defer(() => collection.findOne(query));
+  }
+
+
+
+}
+/**
+ * @returns {ClientDA}
+ */
+module.exports = ClientDA;
