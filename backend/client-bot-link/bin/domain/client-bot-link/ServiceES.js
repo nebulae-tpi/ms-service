ccfb0e490e2b757d07b1a8984fb749a43f53f41d
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2023-02-03 15:29:14 -0500

    Se agrega escuchado de evento asignado de servicios para notificar al chatBot

--- backend/client-bot-link/bin/domain/client-bot-link/ServiceES.js
+++ backend/client-bot-link/bin/domain/client-bot-link/ServiceES.js
@@ -24,6 +24,7 @@ class ServiceES {
    * @param {*} serviceEvent service event
    */
   handleServiceAssignedEvents$(serviceEvent) {
+    console.log("LLEGA EVENTO ASIGNADO ===> ", serviceEvent);
     return ServiceDA.getService$(serviceEvent.aid).pipe(
       tap(service => {
         this.sendTextMessage(`${service.driver.fullname} se dirige para la  dirección ${service.pickUp.addressLine1} en el vehiculo de placas ${service.vehicle.licensePlate}`, service.client.associatedClientPhoneNumber)
