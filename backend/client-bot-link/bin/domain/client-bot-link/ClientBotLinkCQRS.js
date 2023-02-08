6016a6f2b82efa0660a5048eee84567a319977ed
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2023-02-08 15:05:55 -0500

    Se agrega la solicitud de servicios con aire acondicionado

--- backend/client-bot-link/bin/domain/client-bot-link/ClientBotLinkCQRS.js
+++ backend/client-bot-link/bin/domain/client-bot-link/ClientBotLinkCQRS.js
@@ -340,12 +340,12 @@ class ClientBotLinkCQRS {
   continueConversation$(message, conversationContent, client, serviceCount) {
     let content;
     const serviceLimit = parseInt(process.env.SATELLITE_SERVICE_LIMIT || "5");
-    const availableServiceCount = serviceLimit - serviceCount;
     if (((message || {}).text || {}).body) {
-      const charCount = [...message.text.body].filter(c => "🚗🚌🚎🏎🚓🚑🚒🚐🛻🚚🚛🚔🚍🚕🚖🚜🚙🚘🥶❄️⛄🧊".includes(c)).length;
+      const charCount = [...message.text.body].filter(c => "🚗🚌🚎🏎🚓🚑🚒🚐🛻🚚🚛🚔🚍🚕🚖🚜🚙🚘🥶⛄🧊".includes(c)).length;
       let specialCharCount = [...message.text.body].filter(c => "🥶⛄🧊".includes(c)).length;
       const specialDoubleCharCount = [...message.text.body].filter(c => "❄️".includes(c)).length;
-      specialCharCount = specialCharCount + (specialDoubleCharCount / 2)
+      charCount = specialCharCount + (specialDoubleCharCount / 2);
+      specialCharCount = specialCharCount + (specialDoubleCharCount / 2);
       
       if (charCount > 0) {
         return this.requestService$(serviceCount, charCount, specialCharCount, client, conversationContent.waId);
