b8c8e95e8bc41ff06449f9790328898bbf8275d4
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2023-02-13 16:00:09 -0500

    Se agrega el tiempo estimado en el mensaje de asignacion del chatbot

--- backend/client-bot-link/bin/domain/client-bot-link/ServiceES.js
+++ backend/client-bot-link/bin/domain/client-bot-link/ServiceES.js
@@ -37,11 +37,19 @@ class ServiceES {
     return ServiceDA.getService$(serviceEvent.aid).pipe(
       tap(service => {
         if (service.client.phone) {
-          this.sendTextMessage(`${service.driver.fullname} se dirige para la  dirección ${service.pickUp.addressLine1} en el vehículo de placas ${service.vehicle.licensePlate}`, `57${service.client.phone}`)
+          const minutes = this.millisToMinutesAndSeconds(service.pickUpETA - Date.now())
+          this.sendTextMessage(`${service.driver.fullname} se dirige para la  dirección ${service.pickUp.addressLine1} en el vehículo de placas ${service.vehicle.licensePlate}, Tiempo estimado de llegada : ${minutes}`, `57${service.client.phone}`)
         }
       })
     );
   }
+
+  millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+  
 
   /**
    * Handles the service event
