f018ad90b837e805bfa0a98b1cfe8675579d9197
nesas-dev-01 <nesas-dev-01@localhost.localdomain>
2024-08-02 13:45:52 -0500

    Se agrega el referido de cliente a cliente

---
+++ backend/client-bot-link/bin/domain/client-bot-link/data-access/DriverDA.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let mongoDB = undefined;
+//const mongoDB = require('./MongoDB')();
+const CollectionName = "Client";
+const { CustomError } = require("../../../tools/customError");
+const { map, mergeMap, reduce } = require("rxjs/operators");
+const { of, Observable, defer, from } = require("rxjs");
+const Crosscutting = require("../../../tools/Crosscutting");
+
+class DriverDA {
+  static start$(mongoDbInstance) {
+    return Observable.create(observer => {
+      if (mongoDbInstance) {
+        mongoDB = mongoDbInstance;
+        observer.next("using given mongo instance");
+      } else {
+        mongoDB = require("../../../data/MongoDB").singleton();
+        observer.next("using singleton system-wide mongo instance");
+      }
+      observer.complete();
+    });
+  }
+
+  static getDriverByReferredCode$(clientCode, projection) {
+    const collection = mongoDB.db.collection(CollectionName);
+
+    const query = {
+      "clientCode": clientCode
+    };
+    return defer(() => collection.findOne(query, projection));
+  }
+
+}
+/**
+ * @returns {DriverDA}
+ */
+module.exports = DriverDA;
