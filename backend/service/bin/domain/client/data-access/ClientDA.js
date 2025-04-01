98948e4554f45fbdf4814d2c431933763afa019b
Daniel Tobon <daniel.tobon@nebulae.com.co>
2025-04-01 08:18:59 -0500

    se habilitan logs de seguimiento

--- backend/service/bin/domain/client/data-access/ClientDA.js
+++ backend/service/bin/domain/client/data-access/ClientDA.js
@@ -128,6 +128,8 @@ class ClientDA {
     if(businessId){
       update.businessId = businessId;
     }
+    console.log("CliendId updated ===>", id);
+    console.log("Update client ==>", update);
     return defer(()=>
         collection.findOneAndUpdate(
           { _id: id },
